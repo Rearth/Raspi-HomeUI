074f7992ba37f10f55c85a9897314a1cd537e8bc
rearth <davidus2010@gmail.com>
2017-12-01 12:06:12 +0100

    Improved calendar display

--- src/raspi_ui/backend/calendar/CalendarDesign.java
+++ src/raspi_ui/backend/calendar/CalendarDesign.java
@@ -24,6 +24,9 @@ public class CalendarDesign {
     private static final Font font = Font.font("Verdana", 15);
     private static final Font titleFont = Font.font("Verdana", 28);
     private static final List<Node> nodes = new ArrayList<>();
+    private static final String borderStyle = "-fx-border-color: darkgray; -fx-border-radius: 2; -fx-border-width: 1";
+
+    private static int curHeight;
 
     public static TextFlow createLabel(CalendarEvent event) {
         Text dateText = new Text(event.getNiceTime() + " ");
@@ -37,6 +40,12 @@ public class CalendarDesign {
             content += "...";
         }
 
+        curHeight = height;
+        if (content.length() < 14) {
+            curHeight = height / 2;
+        }
+        content += " ";
+
         Text contentText = new Text(content);
         contentText.setStyle("-fx-font-weight: bold; -fx-effect: dropshadow(two-pass-box, black, 4, 0.2, 2, 2)");
         contentText.setFill(Color.DARKGRAY);
@@ -45,6 +54,8 @@ public class CalendarDesign {
         TextFlow flow = new TextFlow();
         flow.getChildren().addAll(dateText, contentText);
         flow.setTextAlignment(TextAlignment.RIGHT);
+
+        flow.setStyle(borderStyle);
 
         return flow;
     }
@@ -56,6 +67,7 @@ public class CalendarDesign {
         System.out.println("settings labels");
 
         int curnum = 0;
+        int curPos = 0;
 
         for (CalendarEvent event : events) {
             TextFlow label = createLabel(event);
@@ -63,22 +75,58 @@ public class CalendarDesign {
             pane.getChildren().add(label);
 
             label.setPrefWidth(width);
+
             if (today) {
                 label.setLayoutX(1024 - 450);
             } else {
                 label.setLayoutX(1024 - 200);
             }
-            label.setLayoutY(600 - height * (1 + curnum) - 10);
+
+            curPos += curHeight + 7;
+            label.setLayoutY(600 - 10  - 4 * curnum - curPos);
+
+            //label.setLayoutY(600 - height * (1 + curnum) - 10);
             System.out.println("creating label for: " + event.getName() + " num= " + curnum + " height: " +  label.getHeight());
 
             curnum++;
         }
 
-        createTitle(pane, today, curnum);
+        if (events.size() == 0 ) {
+
+            curPos += height;
+            createPlaceHolder(pane, today, curnum, curPos);
+            curPos += 12;
+        }
+
+        createTitle(pane, today, curnum, curPos);
 
     }
 
-    private static void createTitle(Pane pane, boolean today, int count) {
+    private static void createPlaceHolder(Pane pane, boolean today, int curnum, int curPos) {
+
+        Label empty = new Label("Keine Termine");
+        nodes.add(empty);
+        empty.setStyle("-fx-font-weight: bold; -fx-effect: dropshadow(two-pass-box, black, 4, 0.2, 2, 2)");
+        empty.setTextFill(Color.DARKGRAY);
+        empty.setFont(font);
+
+        empty.setPrefWidth(180);
+        empty.setAlignment(Pos.CENTER_RIGHT);
+        empty.setTextAlignment(TextAlignment.RIGHT);
+
+        if (today) {
+            empty.setLayoutX(1024 - 450);
+        } else {
+            empty.setLayoutX(1024 - 200);
+        }
+        empty.setLayoutY(600 - 10  - 4 * curnum - curPos);
+
+        curnum++;
+        pane.getChildren().add(empty);
+
+    }
+
+    private static void createTitle(Pane pane, boolean today, int count, int curPos) {
 
         Label title = new Label("Heute");
         nodes.add(title);
@@ -91,7 +139,7 @@ public class CalendarDesign {
         title.setTextAlignment(TextAlignment.RIGHT);
 
         pane.getChildren().add(title);
-        title.setLayoutY(600 - height * (1 + count) - 15);
+        title.setLayoutY(600 - 10  - 4 * count- curPos - height);
         title.setLayoutX(1024 - 450);
 
 
